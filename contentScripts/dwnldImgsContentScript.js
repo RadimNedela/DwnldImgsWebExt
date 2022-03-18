afd28b80dacca5c1a430dee5526cb6d93f3205b2
Radim Neděla <radim.nedela@seznam.cz>
2022-03-18 21:32:56 +0100

    funguje

--- contentScripts/dwnldImgsContentScript.js
+++ contentScripts/dwnldImgsContentScript.js
@@ -1,7 +1,52 @@
 "use strict";
 
-browser.runtime.onMessage.addListener(request => {
+if (!document.__dwnldImgsContentScript) {
+  browser.runtime.onMessage.addListener(request => {
     console.log("Message from the background script:");
     console.log(request.command);
-    return Promise.resolve({response: "Hi from content script"});
+    return Promise.resolve({
+      response: "Hi from content script",
+      imgs: getImgs(),
+      pathname: getPathname(),
+      models: getModels(),
+    });
   });
+
+
+
+
+  function getImgs() {
+    var foundImgs = [];
+    //foundImgs.push("https://cdni.teacherpics.com/1280/1/200/44793016/44793016_004_0e4b.jpg")
+    var links = $("a.ss-image");
+    links.each((index, element) => {
+      //   var a = $(element).find("a");
+      //   if (a.length > 0) {
+      //     var firstA = a[0];
+      var href = element.href;
+      //     if (href.includes(".zip?st=")) {
+      foundImgs.push(href);
+      //     }
+      //   }
+    });
+    return foundImgs;
+  }
+
+  function getPathname() {
+    var pathname = window.location.pathname;
+    //directory = directory.substr(directory.lastIndexOf("/") + 1);
+    return pathname;
+  }
+
+  function getModels() {
+    var models = "";
+    var persons = $(".person");
+    models = persons.text();
+    console.log("found models " + models);
+    return models;
+  }
+
+
+
+  document.__dwnldImgsContentScript = true;
+}
--- popup/chooseAction.js
+++ popup/chooseAction.js
@@ -1,61 +1,70 @@
 "use strict";
 
-if (!__chooseAction) {
-    /**
-    * Listen for clicks on the buttons, and send the appropriate message to
-    * the content script in the page.
-    */
-    function listenForClicks() {
-        document.addEventListener("click", (e) => {
+//if (!__chooseAction) {
+/**
+* Listen for clicks on the buttons, and send the appropriate message to
+* the content script in the page.
+*/
+function listenForClicks() {
+    document.addEventListener("click", (e) => {
 
-            function getImages(tabs) {
-                browser.tabs.sendMessage(tabs[0].id, { command: "getImages" }, downloadFiles)
-            }
+        function getImages(tabs) {
+            browser.tabs.sendMessage(tabs[0].id, { command: "getImages" }, downloadFiles)
+        }
 
-            if (e.target.classList.contains("getImages")) {
-                browser.tabs.query({ active: true, currentWindow: true }, getImages);
-            }
+        if (e.target.classList.contains("getImages")) {
+            browser.tabs.query({ active: true, currentWindow: true }, getImages);
+        }
+    });
+}
+
+/**
+* When the popup loads, inject a content script into the active tab,
+* and add a click handler.
+*/
+browser.tabs.executeScript({ file: "/libs/jquery-3.6.0.min.js" });
+browser.tabs.executeScript({ file: "/contentScripts/dwnldImgsContentScript.js" });
+listenForClicks();
+
+
+
+
+
+function downloadFiles(message) {
+    log(message.response);
+    var fileList = message.imgs;
+    var pathname = message.pathname;
+    var models = message.models;
+
+    //  /pics/schoolgirl-madison-breeze-disrobes-to-show-stepdad-tiny-tits-give-handjob-44793016/
+    var directory;
+    if (models) {
+        directory = models.replace(" ", "_");
+    } else 
+        directory = pathname.substr(6, pathname.length - 1);
+
+    log("directory = " + directory);
+
+    for (let i = 0; i < fileList.length; i++) {
+        var fileName = fileList[i];
+        fileName = fileName.substr(fileName.lastIndexOf("/") + 1);
+        fileName = directory + "/" + fileName;
+
+        log("downloading " + fileList[i] + " as " + fileName);
+        browser.downloads.download({
+            url: fileList[i],
+            filename: fileName
         });
     }
-
-    /**
-    * When the popup loads, inject a content script into the active tab,
-    * and add a click handler.
-    */
-    browser.tabs.executeScript({ file: "/libs/jquery-3.6.0.min.js" });
-    browser.tabs.executeScript({ file: "/contentScripts/dwnldImgsContentScript.js" });
-    listenForClicks();
-
-
-
-
-
-    function downloadFiles(message) {
-        log(message.response);
-        var fileList = message.zips;
-        var directory = message.directory;
-
-        for (let i = 0; i < fileList.length; i++) {
-            var fileName = fileList[i];
-            var zipIndex = fileName.indexOf(".zip?st=") + 4;
-            fileName = fileName.substr(0, zipIndex);
-            fileName = fileName.substr(fileName.lastIndexOf("/") + 1);
-            fileName = directory + "_" + fileName;
-
-            chrome.downloads.download({
-                url: fileList[i],
-                filename: fileName
-            });
-        }
-    }
-
-
-
-    var log = function (msg) {
-        console.error(`: ${msg}`);
-    }
-
-
-    __chooseAction = true;
-    log("loaded");
 }
+
+
+
+var log = function (msg) {
+    console.error(`: ${msg}`);
+}
+
+
+__chooseAction = true;
+log("loaded");
+//}
